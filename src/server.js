9c912f0126ca0a2336f8af5c67fe73f43174f108
Aditya Singh <yeezusgod01@gmail.com>
2025-02-04 12:30:10 +0530

    Optimized cors config for my local host

--- src/server.js
+++ src/server.js
@@ -9,16 +9,18 @@ dotenv.config()
 
 
 const app = express()
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
-app.use(cors(
-    {
-        origin: "http://localhost:5173",
-        credentials: true
-    }
-))
 
 connectToDB()
 const PORT = process.env.PORT || 3000
