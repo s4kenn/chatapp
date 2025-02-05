84cd378e85bc26ad4bf5f432e540d45683e71722
Aditya Singh <yeezusgod01@gmail.com>
2025-02-05 12:11:49 +0530

    Some minor optimization in message routes

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth', authRoutes)
-app.use('/api/message', messageRoutes)
+app.use('/api/messages', messageRoutes)
 
 connectToDB()
 const PORT = process.env.PORT || 3000
